81623da607e4039aad59f3ba027a00444eed7927
Jay <jayakanthr@dckap.com>
2020-10-08 10:57:02 +0530

    day3

--- Training/WelcomeMessage/Model/VisitorLocation.php
+++ Training/WelcomeMessage/Model/VisitorLocation.php
@@ -7,17 +7,40 @@ class VisitorLocation implements VisitorLocationInterface
 {
     private $remoteAddress;
 
+    private $ipApi;
+
+    private $serializer;
+
+    private $locationDTOFactory;
+
     public function __construct(
-        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress
+        \Magento\Framework\HTTP\PhpEnvironment\RemoteAddress $remoteAddress,
+        \Training\WelcomeMessage\Api\IpApiInterface $ipApi,
+        \Magento\Framework\Serialize\SerializerInterface $serializer,
+        \Training\WelcomeMessage\Api\Data\LocationDTOInterfaceFactory $locationDTOFactory
     )
     {
         $this->remoteAddress = $remoteAddress;
+        $this->serializer = $serializer;
+        $this->ipApi = $ipApi;
+        $this->locationDTOFactory = $locationDTOFactory;
     }
 
     public function getAreas()
     {
+        $data = $this->ipApi->lookup($this->getAddress());
+        $jsonData = $this->serializer->unserialize($data);
+
+        return $this->locationDTOFactory->create([
+            "country" => $jsonData["countryCode"],
+            "region" => $jsonData["region"]
+        ]);
+    }
+
+    private function getAddress()
+    {
         $address = $this->remoteAddress->getRemoteAddress();
-        return $address;
+        return $address === "192.168.144.1" ? "8.8.8.8" : $address;
     }
 
 }
